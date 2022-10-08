09e0fa780ef079c008664973a2747f0758b55741
dasol <hurdasol@naver.com>
2022-10-08 11:32:54 +0900

    change UIKit to SwiftUI

---
+++ Hangleton_Team3/Hangleton_Team3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hangleton_Team3
+//
+//  Created by hurdasol on 2022/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hangleton_Team3/Hangleton_Team3/Hangleton_Team3App.swift
@@ -0,0 +1,17 @@
+//
+//  Hangleton_Team3App.swift
+//  Hangleton_Team3
+//
+//  Created by hurdasol on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct Hangleton_Team3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
